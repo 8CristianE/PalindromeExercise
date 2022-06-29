ddba54d25ecad64e88f04979612605c68c7d0f00
cristianespinosa7621@gmail.com <email@example.com>
2022-06-29 09:48:11 -0700

    intitial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool IsAPalindrome(string word)
+        {
+            var reverse = "";
+            for (var i = word.Length - 1; i >= 0; i--)
+            {
+                reverse += word.ToLower()[i];
+            }
+            if (reverse == word.ToLower())
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("mom", true)]
+        [InlineData("Car", false)]
+        public void PalidromeTester(string word, bool expected)
         {
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+
+
 
         }
     }
